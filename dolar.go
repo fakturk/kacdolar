f49be5f410b1b6618310a35f88d406e6a2778aa7
Fatih Akturk <fakturk@ilkes-MacBook-Pro.local>
2018-08-10 22:23:58 +0300

    mock functions added

--- dolar.go
+++ dolar.go
@@ -4,6 +4,8 @@ import (
   	"github.com/gorilla/mux"
     "net/http"
     "log"
+    "strconv"
+
 )
 
 func main()  {
@@ -18,4 +20,23 @@ func main()  {
 func dolarHandler(w http.ResponseWriter, r *http.Request) {
   w.WriteHeader(http.StatusOK)
   w.Write([]byte("I am alive !\n"))
+  asgari := getAsgari()
+  dolar := getDolar()
+  asgariDolar:= asgari / dolar
+  a:=strconv.FormatFloat(asgari,'f', -1, 64)
+  d:=strconv.FormatFloat(dolar,'f', -1, 64)
+  ad:=strconv.FormatFloat(asgariDolar,'f', -1, 64)
+  w.Write([]byte("asgari ucret : "+a+"\n"))
+  w.Write([]byte("dolar : "+d+"\n"))
+  w.Write([]byte("asgari ucret kac dolar : "+ad+"\n"))
+}
+
+func getAsgari() (asgari float64)  {
+  asgari=1725.08
+  return
+}
+
+func getDolar() (dolar float64) {
+  dolar = 6.46
+  return
 }
